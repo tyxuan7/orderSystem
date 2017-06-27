4fdb275c2e906a2b10bd08e125bca0aff898cca2
tang yuxuan <1207414394@qq.com>
2017-06-27 18:15:01 +0800

    订单系统

---
+++ src/main/java/com/orderSystem/Interceptor/OrderInterceptor.java
@@ -0,0 +1,7 @@
+package com.orderSystem.Interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class OrderInterceptor extends HandlerInterceptorAdapter {
+
+}
