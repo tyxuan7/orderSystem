75cd2a9a4e7555b1d4741f91578826e8ac57f4b7
tangyuxuan <1207414394@qq.com>
2017-06-29 23:20:51 +0800

    con

--- src/main/java/com/orderSystem/test/BaseTest.java
+++ src/main/java/com/orderSystem/test/BaseTest.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:applicationContext.xml","classpath:dispatcherServlet-servlet.xml"})
+@ContextConfiguration(locations={"classpath:applicationContext.xml","file:src/main/resources/dispatcherServlet-servlet.xml"})
 public class BaseTest {
 	public static void main(String[] args) {
 	
